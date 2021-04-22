a1685c261613dfb39f7665dfe39fd0b96788b995
joanformacio <joanlmformacio@putlook.es>
2021-04-22 10:36:30 +0200

    create modul 12

---
+++ Nivell3/modul12/src/test/java/com/example/modul12/Modul12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modul12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modul12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
